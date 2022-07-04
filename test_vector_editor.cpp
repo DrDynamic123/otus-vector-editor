464e937c388b264b7458e3bfc78c86c8a9197249
Anton <AntonyPolyakov@yandex.ru>
2022-07-04 21:53:19 +0300

    remove all files for review

--- test_vector_editor.cpp
+++
@@ -1,7 +0,0 @@
-#define TEST_MODULE test_vector_editor
-
-#include "gtest/gtest.h"
-
-
-TEST(vector_editor_test_case, _test) {
-}
--- vector_editor.h
+++
@@ -1,7 +0,0 @@
-#ifndef VECTOR_EDITOR_H
-#define VECTOR_EDITOR_H
-
-
-
-
-#endif // VECTOR_EDITOR_H
